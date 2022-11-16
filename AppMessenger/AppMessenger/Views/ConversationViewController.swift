212055055b1f06befbb28522f34abe33540fa599
Rodrigo <Rodriigo84@gmail.com>
2022-11-16 13:55:33 -0300

    Adicionando arquivos no projeto

---
+++ AppMessenger/AppMessenger/Views/ConversationViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ConversationsViewController.swift
+//  AppMessenger
+//
+//  Created by Rodrigo Garcia on 16/11/22.
+//
+
+import UIKit
+
+class ConversationViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
